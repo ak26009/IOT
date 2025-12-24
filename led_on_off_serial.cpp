937c6b1a928a67cf3bd7086808ffaf0cb82b301c
Atharva Kadam <154217374+ak26009@users.noreply.github.com>
2025-12-24 17:29:43 +0530

    Add LED control via serial commands

---
+++ led_on_off_serial.cpp
@@ -0,0 +1,26 @@
+int ledPin = 18; 
+char command;     
+
+void setup() {
+  pinMode(ledPin, OUTPUT);     
+  Serial.begin(115200);         
+  Serial.println("Enter '1' to turn ON, '0' to turn OFF LED");
+}
+
+void loop() {
+  if (Serial.available() > 0) {   
+    command = Serial.read();       
+    
+    if (command == '1') {
+      digitalWrite(ledPin, HIGH); 
+      Serial.println("LED is ON");
+    }
+    else if (command == '0') {
+      digitalWrite(ledPin, LOW);  
+      Serial.println("LED is OFF");
+    }
+    else {
+      Serial.println("Invalid command! Use '1' or '0'");
+    }
+  }
+}
